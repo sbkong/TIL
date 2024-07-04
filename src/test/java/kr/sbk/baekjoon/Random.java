ee594935d18d38b4fb960e53f7d4c5bc2353590a
sbkong <sbkong3@gmail.com>
2024-07-04 21:12:54 +0900

    문제 풀이

--- src/test/java/kr/sbk/baekjoon/Random.java
+++ src/test/java/kr/sbk/baekjoon/Random.java
@@ -39,6 +39,48 @@ public class Random {
 
         System.out.println(result);
     }
+
+    /*
+    문제
+    정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
+
+    둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
+
+    출력
+    X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
+     */
+    public static void betterThen() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        String number[] = input.split("\\s");
+
+        List list = new ArrayList<>();
+        for (int i = 0; i < Integer.parseInt(number[0]); i++) {
+            list.add((int) (Math.random() * 10000.0));
+        }
+
+        list.stream().forEach(e -> System.out.print(e + " "));
+        System.out.println();
+
+        list.stream().forEach(e -> {
+            if ((int)e < Integer.parseInt(number[1])) System.out.print(e+" ");
+        });
+
+        /***
+         input
+         10 5000
+
+         output
+         8470 2438 5984 889 2421 9328 5613 1176 923 7151
+         2438 889 2421 1176 923
+         */
+    }
+
 }
 
 /*
