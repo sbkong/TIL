fa7431a80ffdce66159f171c0ee81fe2c0f8c70a
sbkong <sbkong3@gmail.com>
2024-07-05 19:03:15 +0900

    Solve Algorithm

---
+++ src/test/java/kr/sbk/baekjoon/BetterThan.java
@@ -0,0 +1,11 @@
+package kr.sbk.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BetterThan {
+
+}
