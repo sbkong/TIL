6cd235946c8dd32fe79bfb0b60a4358db0b15778
sbkong <sbkong3@gmail.com>
2024-07-08 23:37:45 +0900

    Module Update

--- src/test/java/kr/sbk/baekjoon/util/IOTest.java
+++ src/test/java/kr/sbk/baekjoon/util/IOTest.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 
 public abstract class IOTest {
 
-    // 이 메서드를 실행하면 input값의 바이트 배열을 스트림에 담아 System.in에 할당해줍니다.
     public void systemIn(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
     }
