14a558c8a44823f344dc1f097da9c2ef991f78a5
sbkong <sbkong3@gmail.com>
2024-07-25 22:23:18 +0900

    String Study

--- src/test/java/kr/sbk/baekjoon/CommonMath.java
+++ src/test/java/kr/sbk/baekjoon/CommonMath.java
@@ -48,4 +48,34 @@ public class CommonMath {
         }
         return sum;
     }
+
+    /**
+     * Convert base n.
+     * description https://www.acmicpc.net/problem/11005
+     * @throws Exception the exception
+     */
+    public void convertBaseN() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        int base = Integer.parseInt(input[1]);
+        long numbers = Long.parseLong(input[0]);
+
+        StringBuilder convertNumber = new StringBuilder();
+
+        while (numbers >= base) {
+            long remainder = numbers % base;
+            if (remainder > 9) convertNumber.append((char) (remainder + 55));
+            else convertNumber.append(remainder);
+
+            numbers = numbers / base;
+        }
+        long remainder = numbers % base;
+        if (remainder > 9) convertNumber.append((char) (remainder + 55));
+        else convertNumber.append(remainder);
+
+        System.out.println(convertNumber.reverse());
+
+    }
 }
