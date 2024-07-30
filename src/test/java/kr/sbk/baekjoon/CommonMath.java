7e6e340c71121edfc0f1c7a6ea19865f60aaf8e9
sbkong <sbkong3@gmail.com>
2024-07-30 22:40:34 +0900

    분수 찾기 (대각선 이동)

--- src/test/java/kr/sbk/baekjoon/CommonMath.java
+++ src/test/java/kr/sbk/baekjoon/CommonMath.java
@@ -188,7 +188,7 @@ public class CommonMath {
     /**
      * Honey bee house 2.
      */
-    public void honeyBeeHouse2 () {
+    public void honeyBeeHouse2() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         try {
@@ -209,6 +209,106 @@ public class CommonMath {
 
             System.out.println(step);
 
+        } catch (IOException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Search fractal.
+     */
+    public void searchFractal() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            // 입력을 받아서 target 변수에 저장
+            int target = Integer.parseInt(br.readLine());
+
+            // level, 방향 및 초기 좌표 설정
+            int level = 1;
+            int courseX = 1;
+            int courseY = -1;
+            int x = 1, y = 1;
+            int cnt = 0;
+
+            while (true) {
+                // 방향을 반대로 바꿈
+                courseX = -courseX;
+                courseY = -courseY;
+
+                // 방향에 따라 x, y 초기화
+                if (courseX == 1) {
+                    x = 1;
+                    y = level;
+                } else {
+                    x = level;
+                    y = 1;
+                }
+
+                // 내부 루프: 대각선으로 이동하며 좌표를 출력
+                while (true) {
+                    cnt++;
+                    if (cnt == target) {
+                        // 목표 숫자에 도달하면 해당 분수를 출력하고 종료
+                        System.out.println(x + "/" + y);
+                        return;
+                    }
+
+                    // 현재 좌표 출력
+                    if (courseX == 1) {
+                        x++;
+                        y--;
+                        if (y == 0) break;
+                    } else {
+                        x--;
+                        y++;
+                        if (x == 0) break;
+                    }
+                }
+                // 다음 레벨로 증가
+                level++;
+            }
+        } catch (IOException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Search fractal 2.
+     * <p>
+     * 입력 처리: 사용자가 입력한 숫자를 target 변수에 저장합니다.
+     * 레벨 및 카운트 초기화: level과 cnt를 초기화합니다.
+     * 목표 레벨 찾기: cnt + level이 target보다 작거나 같을 때까지 cnt에 level을 더하고 level을 증가시킵니다.
+     * 오프셋 계산: target과 cnt의 차이를 offset에 저장합니다.
+     * 분수 계산: level이 짝수인지 홀수인지에 따라 x와 y를 계산합니다.
+     * 결과 출력: 계산된 x와 y를 분수 형태로 출력합니다.
+     */
+    public void searchFractal2() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            int target = Integer.parseInt(br.readLine());
+
+            int level = 1;
+            int cnt = 1;
+
+            while (cnt + level <= target) {
+                cnt += level;
+                level++;
+            }
+
+            int offset = target - cnt;
+            int x, y;
+
+            if (level % 2 == 0) {
+                x = 1 + offset;
+                y = level - offset;
+            } else {
+                x = level - offset;
+                y = 1 + offset;
+            }
+
+            System.out.println(x + "/" + y);
         } catch (IOException | NumberFormatException e) {
             System.out.println(e.getMessage());
         }
