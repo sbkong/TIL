41cb876e0faa3a4bcd011b071ca4f565bb208b67
sbkong <sbkong3@gmail.com>
2024-08-13 23:51:24 +0900

    Basic geometry

--- src/test/java/kr/sbk/baekjoon/Geometry.java
+++ src/test/java/kr/sbk/baekjoon/Geometry.java
@@ -2,6 +2,7 @@ package kr.sbk.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * The type Geometry.
@@ -39,6 +40,50 @@ public class Geometry {
             int distanceY = h / 2 < y ? h - y : y;
 
             System.out.println(Math.min(distanceX, distanceY));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Where is the last point.
+     */
+    public void whereIsTheLastPoint() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+
+            Scanner sc = new Scanner(System.in);
+
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            int x3 = sc.nextInt();
+            int y3 = sc.nextInt();
+
+            int x4, y4;
+
+            if (x1 == x2) {
+                x4 = x3;
+            } else if (x1 == x3) {
+                x4 = x2;
+            } else {
+                x4 = x1;
+            }
+
+            if (y1 == y2) {
+                y4 = y3;
+            } else if (y1 == y3) {
+                y4 = y2;
+            } else {
+                y4 = y1;
+            }
+
+            System.out.println(x4 + " " + y4);
+
+            sc.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
