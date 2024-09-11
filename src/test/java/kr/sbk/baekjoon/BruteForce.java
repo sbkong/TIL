05732478d9c79fc3ad3ae7486c95daaac648c8d8
sbkong <36612263+sbkong@users.noreply.github.com>
2024-09-11 21:30:36 +0900

    Update BruteForce.java

--- src/test/java/kr/sbk/baekjoon/BruteForce.java
+++ src/test/java/kr/sbk/baekjoon/BruteForce.java
@@ -74,4 +74,28 @@ public class BruteForce {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+    * 연립방정식 해 구하기
+    * https://www.acmicpc.net/problem/19532
+    */
+    public void simultaneousEquation(){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+        int d = Integer.parseInt(input[3]);
+        int e = Integer.parseInt(input[4]);
+        int f = Integer.parseInt(input[5]);
+
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if ((a * x) + (b * y) == c && (d * x) + (e * y) == f) {
+                    System.out.printf("%d %d%n", x, y);
+                }
+            }
+        }
+    }
 }
