51f6f30e96f54b552dbf6f568314627f386c0aa2
sbkong <sbkong@triniti.kr>
2024-09-24 14:33:02 +0900

    - 체스판 잘라서 칠하기

--- src/test/java/kr/sbk/baekjoon/BruteForce.java
+++ src/test/java/kr/sbk/baekjoon/BruteForce.java
@@ -77,9 +77,10 @@ public class BruteForce {
     }
 
     /**
-    * 연립방정식 해 구하기
-    * https://www.acmicpc.net/problem/19532
-    */
+     * 연립방정식 해 구하기
+     * https://www.acmicpc.net/problem/19532
+     * @throws IOException the io exception
+     */
     public void simultaneousEquation() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
@@ -98,5 +99,63 @@ public class BruteForce {
                 }
             }
         }
+    }
+
+
+    /**
+     * Gets refill cell count.
+     * <a href="https://www.acmicpc.net/problem/1018">...</a>
+     */
+    public static void getRefillCellCount() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            // N: 세로 크기, M: 가로 크기
+            String[] size = br.readLine().split(" ");
+            int N = Integer.parseInt(size[0]);
+            int M = Integer.parseInt(size[1]);
+
+            // 보드 입력 받기
+            char[][] board = new char[N][M];
+            for (int i = 0; i < N; i++) {
+                board[i] = br.readLine().toCharArray();
+            }
+
+            int minRepaint = Integer.MAX_VALUE;
+
+            // 가능한 8x8 체스판을 잘라서 확인
+            for (int i = 0; i <= N - 8; i++) {
+                for (int j = 0; j <= M - 8; j++) {
+                    minRepaint = Math.min(minRepaint, countRepaints(board, i, j));
+                }
+            }
+
+            System.out.println(minRepaint);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int countRepaints(char[][] board, int x, int y) {
+        int count1 = 0; // 왼쪽 위가 'W'인 경우
+        int count2 = 0; // 왼쪽 위가 'B'인 경우
+
+        // 8x8 체스판 순회
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                // 현재 칸의 색깔 예상 (패턴1: 왼쪽 위 W, 패턴2: 왼쪽 위 B)
+                char expectedColor1 = (i + j) % 2 == 0 ? 'W' : 'B';
+                char expectedColor2 = (i + j) % 2 == 0 ? 'B' : 'W';
+
+                if (board[x + i][y + j] != expectedColor1) {
+                    count1++;
+                }
+                if (board[x + i][y + j] != expectedColor2) {
+                    count2++;
+                }
+            }
+        }
+
+        return Math.min(count1, count2); // 두 패턴 중 더 적은 칠하기 횟수 반환
     }
 }
